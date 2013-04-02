5185d395cb2fd95da491d9d46b0f846f221b0b8f
Jori Lallo <jtlallo@gmail.com>
2013-04-02 16:53:36 -0700

    Templates

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    
+    return render_template('index.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
