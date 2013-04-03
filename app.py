71fbac0e6a79d550f9ba5766dd54e59356b3bacb
Jori Lallo <jtlallo@gmail.com>
2013-04-02 18:36:33 -0700

    Only for DEBUG locally

--- app.py
+++ app.py
@@ -13,5 +13,6 @@ def index():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.debug = True
+    if os.environ.get('PORT'):
+        app.debug = True
     app.run(host='0.0.0.0', port=port)
